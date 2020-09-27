df2fc88a76a968860aa3b551704da46e22a2248f
Yash Rahurikar <2yashrahurikar@gmail.com>
2020-09-27 14:04:08 +0530

    added the tap utility handler

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const thrush = x => f => f(x);
 
 const pipe = (f, g) => x => g(f(x));
 
+// Tap is responsible for performing some side effect first and then
+// return  whatever we received as an arguement.
+// Works when we want a funnction who is supposed to return void to return something
+// For example console.log return void but if we wish to continue 
+// the pipe line we need that intial x arguement which console log receives.
+const tap = f => x => { f(x); return x };
+
 class Observable {
 	constructor() {
 		this.observers = [];
@@ -23,20 +30,20 @@ class Observable {
 
 // Example 1:
 // Creating an instance of an observable
-const observable = new Observable();
+const observable1 = new Observable();
 
 const double = x => x * 2;
 
 // Passing a function as an observer which is going to subscribe to the observable
-observable.subscribe( 
+observable1.subscribe( 
 	pipe(
+		tap(console.log),
 		double,
-		console.log
 	)
 );
 
 // Emitting some stream of data
-observable.emit(10);
-observable.emit(15);
-observable.emit(25);
-observable.emit(50);
+observable1.emit(10);
+observable1.emit(15);
+observable1.emit(25);
+observable1.emit(50);
